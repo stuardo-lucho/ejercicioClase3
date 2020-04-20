33a5383a4e459f243dbf77283969efaebbc56e27
Stuardo <stuardo.lucho@gmail.com>
2020-04-20 12:08:35 -0500

    primer commit

---
+++ src/test/java/com/ejercicio/clase3/Clase3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicio.clase3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clase3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
